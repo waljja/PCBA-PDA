7796831907137b1d479e528401f9baae8393d558
waljja <2576723618@qq.com>
2022-09-23 15:53:03 +0800

    PCBA移交系统前端

---
+++ README.md
@@ -0,0 +1,48 @@
+﻿# 吉特日化生产管理系统-PDA投料防差错系统
+该程序是基于H5开发的安卓PDA程序,主要用于对接吉特日化生产管理系统投料防差错现场作业。作业模式采用二维码扫描确认模式，可以将人员,物料,设备有效的结合在一起。
+
+# 其他开源软件
+
+### 吉特仓储管理系统基础版本
+https://github.com/hechenqingyuan/gitwms.git
+
+### 吉特仓储管理系统云服务版本
+https://github.com/hechenqingyuan/yun_gitwms.git
+
+### 吉特标签打印小程序
+https://github.com/hechenqingyuan/gitprint
+
+### UPS物流快递C#版SDK,国内最全版本
+https://github.com/hechenqingyuan/UPS.SDK
+
+### UPS物流快递C#版SDK,国内最全版本
+https://github.com/hechenqingyuan/UPS.SDK
+
+### 吉特日化生产执行制造系统 物流管理PDA程序
+https://github.com/hechenqingyuan/GitMes_PDA.git
+
+
+
+# 主要功能
+1. 称量清场:用于清点称量系统称量之后的所有物料
+2. 产线收料:称量间称完物料之后送往生产车间收料确认使用
+3. 开始作业:扫描工单上的二维码确定当前作业内容,并且可以将配方工艺数据自动下发到指定的设备上,实现自动化下配方。
+4. 投料复核:用于生产过程中投料过程顺序的控制,投料设备的核验，以及操作人员的记录
+5. 生产报产:用于记录当前生产批次的最终成品产出量
+
+## 连接服务
+服务地址:http://mesapi.gitwms.com
+演示地址:http://mes.gitwms.com
+
+# 联系方式
+
+手机：15800466429 同微信
+
+QQ：821865130 
+
+技术交流群：142050808  ;  88718955
+
+博客地址:http://www.cnblogs.com/qingyuan/
+
+<img src="http://yun.gitwms.com/Weixin.jpg" width="350" height="500" alt=""/>
+<img src="http://yun.gitwms.com/Alipay.jpg" width="350" height="500" alt=""/>
---
+++ js/bkuiToast.js
@@ -0,0 +1,78 @@
+﻿;(function ($){
+    $.extend({
+        bkuiToast:function (options){
+            var defaults = {
+                content:'',
+                iconHtml:'<i class="fa fa-spinner fa-spin"></i>',
+                themeClass:'',
+                timeout: 0,
+                onShow:null,
+                onClose:null
+            }
+            var options = $.extend({},defaults,options);
+            var bkuiToast={};
+
+            bkuiToast.createBox = function (iconHtml,content) {
+                var minToast = '';
+                var that = this;
+                if (!content && iconHtml){
+                    minToast = ' bkui-mini-toast';
+                }else if (!iconHtml && content){
+                    minToast = ' bkui-text-toast';
+                }
+
+                var $box = $('<div class="bkui-toast'+minToast+'"></div>');
+                var sMask = '<div class="bkui_mask_transparent"></div>'; 
+                var sContentBox = '<div class="bkui-toast-wrapper"><div class="bkui-toast-icon"><div class="bkui-loading-icon">'+iconHtml+'</div></div><div class="bkui-toast-content">'+content+'</div></div>' ;
+
+                $box.html(sMask+sContentBox);
+                $box.addClass(options.themeClass);
+                $box.appendTo($('body'));
+
+                if (options.timeout && (typeof options.timeout == 'number')){
+                    setTimeout(function(){
+                        that.hide();
+                    },options.timeout)
+                }
+                return $box;
+            };
+            bkuiToast.show = function (){
+                $box.css({'display':'block'}).appendTo($('body'));
+                if(typeof options.onShow=='function'){
+                    options.onShow();
+                }
+                return bkuiToast;
+            };
+            bkuiToast.hide = function () {
+                if (typeof arguments[0]=='boolean' || arguments[0]=="true") {
+                    $box.remove();
+                    bkuiToast.closeFn();
+                    bkuiToast = null;
+                }else{
+                    $box.hide(0,function (){
+                        bkuiToast.closeFn();
+                    });
+                }
+            };
+            bkuiToast.closeFn = function (){
+                if(typeof options.onClose=='function'){
+                    options.onClose();
+                }
+            };
+            bkuiToast.setContent = function (content) {
+                $box.find('.bkui-toast-content').text(content);
+                return bkuiToast;
+            };
+            bkuiToast.setIcon = function (content) {
+                $box.find('.bkui-loading-icon').html(content);
+                return bkuiToast;
+            };
+            bkuiToast.destroy = function () {
+                $box.remove();
+                bkuiToast = null;
+            };
+            var $box = bkuiToast.createBox(options.iconHtml,options.content);
+            return bkuiToast;
+        }
+    })
+})(jQuery)
